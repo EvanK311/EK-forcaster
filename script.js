32f9bc69592aa32777bfdf6966f14100c756ac25
Evan K <EvanK311@gmail.com>
2020-09-21 22:57:49 -0700

    working submit button with city search for API results

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var city = "Seattle"
+var cityName = $("<div>")
+var cityTemp = $("<div>")
+var cityHumid = $("<div>")
+var cityWind = $("<div>")
+var cityUV = $("<div>")
 
-queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&cnt=5&appid=941384f3ad9319cea1d15e58a1f228c4"
+// var city = $("#city-input")
+var city
+queryURL = 
+console.log("what up")
 
-$.ajax({
-url: queryURL,
-method: "GET"
-}).then(function (response) {
-    allDaWeather = [response.name, response.main.humidity, response.main.temp, response.wind.speed, response.weather[0].icon]
+$("#find-city").on("click", function (search) {
+    search.preventDefault()
     
-    console.log(response)
+    var city = $("#city-input").val()
+    console.log(city)
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&cnt=5&appid=941384f3ad9319cea1d15e58a1f228c4",
+        method: "GET"
+    }).then(function (response) {
+        
+        allDaWeather = [response.name, response.main.humidity, response.main.temp, response.wind.speed, response.weather[0].icon]
+        
+        console.log(response)
+    
+    })
 })
+
 
