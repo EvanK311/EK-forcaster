0b6ae5186315203da37e21ee3f824f83ff09d4b8
Evan K <EvanK311@gmail.com>
2020-09-23 23:33:29 -0700

    second API generating information and touched up HTML appearance

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // empty vars for latitude and longitude
 var cityLat
 var cityLon
+
 console.log("never give up, yo")
 $("#currentDate").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
@@ -11,10 +12,10 @@ $("#currentDate").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 // on click button to retrieve info
 $("#find-city").on("click", function (search) {
     search.preventDefault()
-    
+
     var city = $("#city-input").val()
     console.log(city)
-    
+
     // ajax call to get current weather of city
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&cnt=5&appid=941384f3ad9319cea1d15e58a1f228c4",
@@ -22,32 +23,37 @@ $("#find-city").on("click", function (search) {
     }).then(function (response) {
         cityName = response.name
         cityTemp = ((response.main.temp) * 1.8 - 459.67).toFixed(0)
-        cityWind = response.wind.speed
+        cityWind = ((response.wind.speed) * .6214).toFixed(1)
         cityHumid = response.main.humidity
-        cityLat = response.coord.lon
-        cityLon = response.coord.lat
+        cityLat = response.coord.lat
+        cityLon = response.coord.lon
         curDate = response.dt
         
-        
+
         allDaWeather = [response.name, response.main.humidity, response.main.temp, response.wind.speed, response.weather[0].icon]
-        
+
         $("#cityName").text("Location: " + cityName)
-        $("#cityTemp").text(cityTemp + " F")
-        $("#cityWind").text(cityWind)
-        $("#cityHumid").text(cityHumid + "%")
+        $("#cityTemp").text("Temperature: " + cityTemp + " F")
+        $("#cityWind").text("Wind speed: " + cityWind + "mph")
+        $("#cityHumid").text("Humidity: " + cityHumid + "%")
+        
         $("currentDate").text(curDate)
 
-        console.log(response)                   
-    })
-      
-    $.ajax({
-        url: `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=941384f3ad9319cea1d15e58a1f228c4`,
+        console.log(response)
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=f5f7477c35e4d4619d2d7dc9decedbd0`,
             method: "GET"
         }).then(function (response) {
             console.log(response)
-        
+            var cityUV = response.daily[0].uvi
+            $("#cityUV").text("UV Index: " + cityUV)
+            $("#cityTemp1").text("Temperature: " + cityTemp + " F")
+
         })
-    
+    })
+
+
+
 
 })
 
